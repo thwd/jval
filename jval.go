1c7d7b58150d78d71b60dae9856f10752d933c8e
Tom <sedevelopers01+thwd@gmail.com>
2016-03-21 23:41:04 +0100

    changed Error to be a struct, interface made no sense

--- jval.go
+++ jval.go
@@ -6,30 +6,18 @@ import (
 	"strconv"
 )
 
-type Error interface {
-	Error() string
-	Field() string
-	Context() interface{}
+type Error struct {
+	Label   string
+	Field   string
+	Context interface{}
+}
+
+// Error implements native "error" interface
+func (e Error) Error() string {
+	return e.Label
 }
 
 var noErrors = []Error{}
-
-type err struct {
-	e, f string
-	c    interface{}
-}
-
-func (e err) Error() string {
-	return e.e
-}
-
-func (e err) Field() string {
-	return e.f
-}
-
-func (e err) Context() interface{} {
-	return e.c
-}
 
 type Validator interface {
 	Validate(value *jsem.Value, field string) []Error
@@ -49,33 +37,33 @@ var String Validator = Lambda(func(v *jsem.Value, f string) []Error {
 	if v.IsString() {
 		return noErrors
 	}
-	return []Error{err{"value_must_be_string", f, nil}}
+	return []Error{Error{"value_must_be_string", f, nil}}
 })
 
 var Number Validator = Lambda(func(v *jsem.Value, f string) []Error {
 	if v.IsNumber() {
 		return noErrors
 	}
-	return []Error{err{"value_must_be_number", f, nil}}
+	return []Error{Error{"value_must_be_number", f, nil}}
 })
 
 var Boolean Validator = Lambda(func(v *jsem.Value, f string) []Error {
 	if v.IsBoolean() {
 		return noErrors
 	}
-	return []Error{err{"value_must_be_boolean", f, nil}}
+	return []Error{Error{"value_must_be_boolean", f, nil}}
 })
 
 var Null Validator = Lambda(func(v *jsem.Value, f string) []Error {
 	if v.IsNull() {
 		return noErrors
 	}
-	return []Error{err{"value_must_be_null", f, nil}}
+	return []Error{Error{"value_must_be_null", f, nil}}
 })
 
 var NotNull Validator = Lambda(func(v *jsem.Value, f string) []Error {
 	if v.IsNull() {
-		return []Error{err{"value_must_not_be_null", f, nil}}
+		return []Error{Error{"value_must_not_be_null", f, nil}}
 	}
 	return noErrors
 })
@@ -109,12 +97,12 @@ func Or(vs ...Validator) Validator {
 func Object(d map[string]Validator) Validator {
 	return Lambda(func(v *jsem.Value, f string) []Error {
 		if !v.IsObject() {
-			return []Error{err{"value_must_be_object", f, nil}}
+			return []Error{Error{"value_must_be_object", f, nil}}
 		}
 		ae := make([]Error, 0, len(d))
 		v.ObjectForEach(func(k string, u *jsem.Value) {
 			if _, ok := d[k]; !ok {
-				ae = append(ae, err{"unexpected_object_key", f + "." + k, nil})
+				ae = append(ae, Error{"unexpected_object_key", f + "." + k, nil})
 			}
 		})
 		for k, a := range d {
@@ -128,7 +116,7 @@ func Object(d map[string]Validator) Validator {
 func Array(e Validator) Validator {
 	return Lambda(func(v *jsem.Value, f string) []Error {
 		if !v.IsArray() {
-			return []Error{err{"value_must_be_array", f, nil}}
+			return []Error{Error{"value_must_be_array", f, nil}}
 		}
 		ae := make([]Error, 0, 8)
 		v.ArrayForEach(func(i int, u *jsem.Value) {
@@ -145,7 +133,7 @@ func Regex(r *regexp.Regexp) Validator {
 			return noErrors
 		}
 		return []Error{
-			err{"value_must_match_regex", f, map[string]string{"regex": r.String()}},
+			Error{"value_must_match_regex", f, map[string]string{"regex": r.String()}},
 		}
 	}))
 }
@@ -158,6 +146,15 @@ func Optional(a Validator) Validator {
 		return a.Validate(v, f)
 	})
 }
+
+// func Relabel(l string, a Validator) Validator {
+// 	return Lambda(func(v *jsem.Value, f string) []Error {
+// 		es := a.Validate(v, f)
+// 		for i, e := range es {
+// 			e
+// 		}
+// 	})
+// }
 
 func LengthBetween(x, y int) Validator {
 	if y < x {
@@ -172,7 +169,7 @@ func LengthBetween(x, y int) Validator {
 		}
 		if l < x || l > y {
 			return []Error{
-				err{"value_must_have_length_between", f, map[string]int{"min": x, "max": y}},
+				Error{"value_must_have_length_between", f, map[string]int{"min": x, "max": y}},
 			}
 		}
 		return noErrors
@@ -191,7 +188,7 @@ func NumberBetween(x, y float64) Validator {
 		l, _ := v.Float64()
 		if l < x || l > y {
 			return []Error{
-				err{"value_must_have_value_between", f, map[string]float64{"min": x, "max": y}},
+				Error{"value_must_have_value_between", f, map[string]float64{"min": x, "max": y}},
 			}
 		}
 		return noErrors
@@ -201,7 +198,7 @@ func NumberBetween(x, y float64) Validator {
 func Exactly(j *jsem.Value) Validator {
 	return Lambda(func(v *jsem.Value, f string) []Error {
 		if !v.Equals(j) {
-			return []Error{err{"value_not_matched_exactly", f, nil}}
+			return []Error{Error{"value_not_matched_exactly", f, nil}}
 		}
 		return noErrors
 	})
